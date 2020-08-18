15d1a7be19f0f0f1ade147087bc4b290723b6c2f
shakhawatfci <94shakha.18@gamil.com>
2020-08-18 11:15:09 +0600

    dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\AllStatic;
+use App\Equipement;
+use App\EquipmentType;
+use App\Project;
+use App\Vendor;
+use App\CarAssign;
+use App\Operator;
+use App\ProjectClaim;
+use DB;
 
 class DashboardController extends Controller
 {
@@ -15,7 +24,28 @@ class DashboardController extends Controller
     public function index()
     {
 
-       
+        // $assigned_equipments = CarAssign::with(
+        //     [
+        //         'equipement',
+        //         'equipment_type:id,name',
+        //         'vendor:id,vendor_name',
+        //         'project:id,project_name',
+        //         'latest_bill:id,bill_no,assign_id,month,date'
+        //     ])
+        //     ->orderBy('updated_at','desc'); 
+
+        // $assigned_equipments = $assigned_equipments->get();
+
+        // DB::table('car_assign')->leftJoin('project_claim',);
+
+
+        // $last_month = date('Y-m',strtotime('-1 month'));
+        // return $claim = ProjectClaim::select('assign_id')->where('month','!=',$last_month)
+        // ->distinct()->get();
+        // $assign = DB::table('car_assign')
+        //     ->whereNotIn('id',$claim)->get();
+
+        // return view('dashboard.dashboard',['assigned_equipments' => $assign]);
         return view('dashboard.dashboard');
     }
 
